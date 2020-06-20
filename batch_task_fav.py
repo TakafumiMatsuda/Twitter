d8f980a0ba6a1a1e35da6169ac80ed84eb88b195
TakafumiMatsuda <rachmaninov0104@gmail.com>
2020-06-21 02:31:20 +0900

    save

--- batch_task_fav.py
+++ batch_task_fav.py
@@ -41,11 +41,3 @@ batch_task2()
 
 # if __name__ == '__main__':
 #     batch_task_twitter(words1, favs)
-
-
-# ps auxww | grep batch_task_twitter.py
-# matsudatakafumi  37938   0.0  0.1  4326236  23072 s000  S+   日02PM   0:43.42 /Library/Developer/CommandLineTools/Library/Frameworks/Python3.framework/Versions/3.7/Resources/Python.app/Contents/MacOS/Python batch_task_twitter.py
-# matsudatakafumi  83526   0.0  0.0  4268192    516 s002  R+    4:04AM   0:00.00 grep batch_task_twitter.py
-# kill -KILL 37938 
-# 2020-04-28 03:56:51.211558
-# zsh: killed     python3 batch_task_twitter.py
